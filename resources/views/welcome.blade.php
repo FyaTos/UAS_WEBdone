358b5e77146c5c2f7eda46a8c02ab7d1ecd2b3f3
kinyol-code <rifkypras007@gmail.com>
2024-05-20 13:56:34 +0700

    MK3

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Neogym</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="{{asset('neogym-html/css/bootstrap.css')}}" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="{{asset('neogym-html/css/style.css')}}" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="{{asset('neogym-html/css/responsive.css')}}" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Neogym
+            </span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+               
+              
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section position-relative">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="col-lg-10 col-md-11 mx-auto">
+                <div class="detail-box">
+                  <div>
+                    <h3>
+                      Fitness
+                    </h3>
+                    <h2>
+                      Training
+                    </h2>
+                    <h1>
+                      Neogym
+                    </h1>
+                    <p>
+                      Welcome to Neo Gym, where your commitment to health and fitness is our top priority. Our website is designed to provide an exceptional user experience, featuring various elements and advantages that will support your journey towards a healthier and more active lifestyle.                    </p>
+                    <div class="">
+                      <a href="login">
+                        Login
+                      </a>
+                      <a href="register">
+                        Register
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+           
+           
+         
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+
+
+  <!-- Us section -->
+
+  <section class="us_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Why Choose Us
+        </h2>
+      </div>
+
+      <div class="us_container ">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-1.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  QUALITY EQUIPMENT
+                </h5>
+                <p>
+                  Our facility is equipped with state-of-the-art fitness equipment to ensure you have the best tools for your workouts.   </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-4.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  NUTRITION
+                </h5>
+                <p>
+                  At Neo Gym, we believe that proper nutrition is a cornerstone of any successful fitness journey. Our website offers access to expert nutritional advice and personalized healthy diet plans designed to complement your fitness routine.                 </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-2.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  HEALTHY DIET PLAN
+                </h5>
+                <p>
+                  Our nutritionists work closely with you to develop a diet plan that aligns with your goals, whether it's weight loss, muscle gain, or overall health improvement. These plans are based on the latest scientific research to ensure optimal results.                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-3.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  SPORT TRAINING
+                </h5>
+                <p>
+                  For those looking to enhance their athletic performance, Neo Gym offers specialized sport training programs. These programs are crafted by experienced trainers to improve your strength, agility, and endurance, tailored to your specific sport or fitness goals.                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end us section -->
+
+
+  <!-- heathy section -->
+
+  <section class="heathy_section layout_padding">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12 mx-auto">
+          <div class="detail-box">
+            <h2>
+              HEALTHY MIND, HEALTHY BODY
+            </h2>
+            <p>
+              <strong>Your body can stand almost anything. It’s your mind that you have to convince.</strong>
+            </p>
+            <div class="btn-box">
+              <a href="">
+                READ MORE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- end heathy section -->
+
+  <!-- trainer section -->
+
+  <section class="trainer_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Our Gym Trainers
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6 mx-auto">
+          <div class="box">
+            <div class="name">
+              <h5>
+                Mr.Gugun
+              </h5>
+            </div>
+            <div class="img-box">
+              <img src="images/t1.jpg" alt="">
+            </div>
+            <div class="social_box">
+              <a href="">
+                <img src="images/facebook-logo.png" alt="">
+              </a>
+              <a href="">
+                <img src="images/twitter.png" alt="">
+              </a>
+              <a href="">
+                <img src="images/instagram-logo.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mx-auto">
+          <div class="box">
+            <div class="name">
+              <h5>
+                Mr.Jaki
+              </h5>
+            </div>
+            <div class="img-box">
+              <img src="images/t2.jpg" alt="">
+            </div>
+            <div class="social_box">
+              <a href="">
+                <img src="images/facebook-logo.png" alt="">
+              </a>
+              <a href="">
+                <img src="images/twitter.png" alt="">
+              </a>
+              <a href="">
+                <img src="images/instagram-logo.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mx-auto">
+          <div class="box">
+            <div class="name">
+              <h5>
+Mr. Sugy
+              </h5>
+            </div>
+            <div class="img-box">
+              <img src="images/t3.jpg" alt="">
+            </div>
+            <div class="social_box">
+              <a href="">
+                <img src="images/facebook-logo.png" alt="">
+              </a>
+              <a href="">
+                <img src="images/twitter.png" alt="">
+              </a>
+              <a href="">
+                <img src="images/instagram-logo.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end trainer section -->
+
+   
+
+  <!-- info section -->
+  <section class="info_section layout_padding2">
+    <div class="container">
+      <div class="info_items">
+        <a href="">
+          <div class="item ">
+            <div class="img-box box-1">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+                Location
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div class="item ">
+            <div class="img-box box-2">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+                +02 1234567890
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div class="item ">
+            <div class="img-box box-3">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+                gymneo@gmail.com
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info_section -->
+
+  <!-- footer section -->
+  <footer class="container-fluid footer_section">
+    <p>
+      &copy; 2020 All Rights Reserved. Design by
+      <a href="https://html.design/">Neo Gym DevTeam</a>
+    </p>
+  </footer>
+  <!-- footer section -->
+
+  <script src="{{asset('neogym-html/js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{asset('neogym-html/js/bootstrap.js')}}"></script>
+
+</body>
+
+</html>
